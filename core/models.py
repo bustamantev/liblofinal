c49bf22844339c3d8941a4fe397382cfdf8ffec4
VaneSuazoU <vanessa.suazo@cl.ey.com>
2024-05-05 01:53:14 -0400

    Ultimas modificaciones

--- core/models.py
+++ core/models.py
@@ -14,7 +14,6 @@ class CustomUser(AbstractUser):
 
 class CategoriaLibro(models.Model):
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
